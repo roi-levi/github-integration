6f5ae43b5885f0c6188595839c934dcb0c2d6afe
Roi Levi <roi.levi@applitools.com>
2018-12-17 16:17:47 +0200

    Update PR

--- MergeDemo.cs
+++ MergeDemo.cs
@@ -14,7 +14,9 @@
         [TestCase(1024, 768)]
         public void TestHelloWorld(int width, int height)
         {
-            var eyes = new Eyes(new Uri(@"https://2378c4e2.ngrok.io"));
+            //var eyes = new Eyes(new Uri(@"https://roiscripteyes.applitools.com"));
+            var eyes = new Eyes(new Uri(@"https://21e2b2b4.ngrok.io"));
+        
             var driver = GetWebDriver();
              
             try 
