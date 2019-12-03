058e52590672b5ba5c6cc1568c7a5d7a31f5127a
Roi Levi <roi.levi@applitools.com>
2019-12-03 11:42:09 +0200

    Update URL

--- MergeDemo.cs
+++ MergeDemo.cs
@@ -14,7 +14,7 @@
         [TestCase(1024, 768)]
         public void TestHelloWorld(int width, int height)
         {
-            var eyes = new Eyes(new Uri(@"https://d19eafa6.ngrok.io"));
+            var eyes = new Eyes(new Uri(@"https://398bbbdd.ngrok.io"));
         
             var driver = GetWebDriver(); 
               
