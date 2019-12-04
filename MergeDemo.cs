77d91b8af9844169ecd114d42bd4fdf24f489f7d
Roi Levi <roi.levi@applitools.com>
2019-12-04 13:32:52 +0200

    Dummy change

--- MergeDemo.cs
+++ MergeDemo.cs
@@ -14,7 +14,7 @@
         [TestCase(1024, 768)]
         public void TestHelloWorld(int width, int height)
         {
-            var eyes = new Eyes(new Uri(@"https://7ca50b5d.ngrok.io")); 
+            var eyes = new Eyes(new Uri(@"https://7ca50b5d.ngrok.io"));
         
             var driver = GetWebDriver();
               
